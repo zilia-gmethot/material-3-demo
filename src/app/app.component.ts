fa1c0b27690f150f9026fd1be3c18d02c46c13ca
Gabriel Méthot <gabriel.methot@ziliahealth.com>
2024-06-10 10:07:09 -0400

    create directive to adjust icon buttons sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { ButtonSizeDirective } from './button-size.directive';
+import { IconButtonSizeDirective } from './icon-button-size-directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, ButtonSizeDirective],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, ButtonSizeDirective, IconButtonSizeDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
